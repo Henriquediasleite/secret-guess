9bf759fd0f9543412bbbeed2422c4a261c2b36f0
Henrique Dias <hdias8969@gmail.com>
2025-01-15 14:18:49 -0300

    Fazendo testes para exibir a mensagem falada no console

--- scripts/reconhecimentoDeVoz.js
+++ scripts/reconhecimentoDeVoz.js
@@ -4,3 +4,7 @@ window.SpeechRecognition || window.webkitSpeechRecognition
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
 recognition.start()
+
+recognition.addEventListener('result', (event) => {
+    console.log(event.results[0][0].transcript)
+})
