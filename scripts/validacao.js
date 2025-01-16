970c2a77c07f0a597bdd728b4cfd000ca3331745
Henrique Dias <hdias8969@gmail.com>
2025-01-16 15:10:43 -0300

    Arrumando erro de sintaxe, e alterando escrita na tela após o chute

--- scripts/validacao.js
+++ scripts/validacao.js
@@ -2,11 +2,28 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute
 
     if(chuteForInvalido(numero)) {
-        elementoChute.innerHTM += '<div>Valor inválido</div>'
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
     }
 
     if(numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-        elementoChute.innerHTM += `<div.Valor inválido: O número secreto precisa estar entre ${menorValor} e ${maiorValor}`
+        elementoChute.innerHTML += `<div>Valor inválido: O número secreto precisa estar entre ${menorValor} e ${maiorValor}`
+        return
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else{
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-up-long"></i></div>
+        `
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era: ${numeroSecreto}</h3>
+        `
     }
 }
 
